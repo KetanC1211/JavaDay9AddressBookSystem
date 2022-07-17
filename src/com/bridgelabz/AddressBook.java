f072a176737829a7916b8181e0bca7f3ae85bff9
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-17 12:54:10 +0530

    Refactor UC2

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -24,23 +24,49 @@ public class AddressBook {
 		System.out.println("Enter Zip");
 		zip=sc.nextInt();
 		sc.close();
-		}
+	}
 
 	void display() {
-	System.out.println(firstName+" "+lastName+" Details");
-	System.out.println("Address: "+address);
-	System.out.println("City: "+city);
-	System.out.println("State: "+state);
-	System.out.println("Zip: "+zip);
-	System.out.println("Phone Number: "+phoneNumber);
-	System.out.println("Email Id: "+emailId);
+		System.out.println(firstName+" "+lastName+" Details");
+		System.out.println("Address: "+address);
+		System.out.println("City: "+city);
+		System.out.println("State: "+state);
+		System.out.println("Zip: "+zip);
+		System.out.println("Phone Number: "+phoneNumber);
+		System.out.println("Email Id: "+emailId);
 	}
-	
+
+	public void ChangingFirstName (String n){
+		firstName = n; 
+	}
+	public void ChangingLastName (String n){
+		lastName = n; 
+	}
+	public void ChangingAddress (String n){
+		address = n; 
+	}
+	public void ChangingCity(String n){
+		city = n; 
+	}
+	public void ChangingState(String n){
+		state = n; 
+	}
+	public void ChangingEmailName (String n){
+		emailId = n; 
+	}
+	public void ChangingPhoneNumber (String n){
+		phoneNumber = n; 
+	}
+	public void ChangingZip (int n){
+		zip = n; 
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address book Program");
 		AddressBook Amit = new AddressBook();
 		Amit.getDetails();
 		Amit.display();
+		Amit.ChangingPhoneNumber("9453857461");
+		Amit.display();
 	}
-
 }
